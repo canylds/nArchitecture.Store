1aeda072228effb0eb91981a6897b330aca5ff23
canyo <canyoldas5410@gmail.com>
2025-02-25 16:16:18 +0300

    Added ProductVariantManager

---
+++ src/store/Application/Services/ProductVariantService/IProductVariantService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Services.ProductVariantService;
+
+public interface IProductVariantService
+{
+    Task<ICollection<ProductVariant>> AddRangeAsync(ICollection<ProductVariant> productVariants);
+}
---
+++ src/store/Application/Services/ProductVariantService/ProductVariantManager.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.ProductVariantService;
+
+public class ProductVariantManager : IProductVariantService
+{
+    private readonly IProductVariantRepository _productVariantRepository;
+
+    public ProductVariantManager(IProductVariantRepository productVariantRepository)
+    {
+        _productVariantRepository = productVariantRepository;
+    }
+
+    public async Task<ICollection<ProductVariant>> AddRangeAsync(ICollection<ProductVariant> productVariants)
+    {
+        ICollection<ProductVariant> addedProductVariants = await _productVariantRepository.AddRangeAsync(productVariants);
+
+        return addedProductVariants;
+    }
+}
